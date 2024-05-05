365cf80797ee0c2ebe846564bdb52efe935d1b1a
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-05-05 14:22:24 +0200

    zad3 - moved everything into try/catch block in Main.java

--- Main.java
+++ Main.java
@@ -11,17 +11,17 @@ public class Main {
             Person person = new Person("Aurora Bennett", 30);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
+            
+            int a = 15;
+            int b = 48;
+            int sum = MathUtils.add(a, b);
+            System.out.println("Sum: " + sum);
+    
+            Messenger messenger = new EmailMessenger();
+            messenger.sendMessage("\nSum of " + a + " and " + b + " is equal to: " + sum);
         } catch (InvalidAgeException e) {
             System.err.println("Error: " + e.getMessage());
         }
 
-        int a = 15;
-        int b = 48;
-
-        int sum = MathUtils.add(a, b);
-        System.out.println("Sum: " + sum);
-
-        Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("\nSum of " + a + " and " + b + " is equal to: " + sum);
     }
 }
