614217b75b826c574512547a402ac0a6012aa633
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-05-05 13:56:17 +0200

    zad2 - first version

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
 
@@ -11,5 +13,8 @@ public class Main {
         } catch (InvalidAgeException e) {
             System.err.println("Error: " + e.getMessage());
         }
+
+        Messenger messenger = new EmailMessenger();
+        messenger.sendMessage("Sample message");
     }
 }
